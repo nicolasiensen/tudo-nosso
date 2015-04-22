9e924b783fbcd6867c1e7147b239cb88a0fca7e8
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-21 22:20:08 -0300

    [#90003756] changed default contribution to json method

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,3 +1,8 @@
 class Contribution < ActiveRecord::Base
+  belongs_to :user
   validates :body, :user_id, :document_id, :paragraph_hash, presence: true
+
+  def to_json options={}
+    super(include: { user: {except: [:api_token, :email]}})
+  end
 end
