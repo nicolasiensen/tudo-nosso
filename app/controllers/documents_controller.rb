7a6667f91d174fe67ac7a727ed62e51b7452d841
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-22 20:19:53 -0300

    [#90003756] refactored the document show view

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,6 @@
 class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
+    @user_api_token = current_user.present? ? current_user.api_token : nil
   end
 end
