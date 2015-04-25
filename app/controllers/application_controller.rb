1231c960005181a23ffbd7aed4c36115bc0a05ad
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-25 10:57:29 -0300

    [#90003756] refactored application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,7 @@ class ApplicationController < ActionController::Base
   after_filter :store_location
 
   def store_location
-    return unless request.get?
-    if (request.path != "/users/sign_in" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        !request.xhr?)
-      session[:previous_url] = request.fullpath
-    end
+    session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
   end
 
   def after_sign_in_path_for(resource)
