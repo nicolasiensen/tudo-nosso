9aedc7b14003f93f4bf619512c27ac5b3c801f14
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-05-30 09:23:00 -0300

    [#90003966] customize devise registration views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   after_filter :store_location
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def store_location
     session[:previous_url] = request.fullpath unless request.fullpath =~ /\/users/
@@ -11,5 +12,11 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     session[:previous_url] || root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
   end
 end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "SignUp", type: :feature do
+  scenario "when the form is valid" do
+    visit new_user_registration_path
+    fill_in "user[first_name]", with: "Nícolas"
+    fill_in "user[last_name]", with: "Iensen"
+    fill_in "user[email]", with: "nicolas@trashmail.com"
+    fill_in "user[password]", with: "12345678"
+    fill_in "user[password_confirmation]", with: "12345678"
+    click_button "Criar conta"
+
+    expect(current_path).to be_eql(root_path)
+    expect(page).to have_css("#current-user img[title='Nícolas Iensen']")
+  end
+
+  scenario "when the form is invalid" do
+
+  end
+end
