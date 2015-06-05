49ca3a766de6f712b318944021ff1afb677a885d
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-06-05 14:47:35 -0300

    [#90003966] fix tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before { User.make! }
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :email }
