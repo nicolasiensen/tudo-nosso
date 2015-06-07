c289da3d9f903c3548521ef75e540995e76e9e17
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-06-07 16:14:02 -0300

    [#90003710] add Document#closed_for_contribution?

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,9 @@
 class Document < ActiveRecord::Base
   validates :body, presence: true
+  belongs_to :user
+  has_many :contributions
+
+  def closed_for_contribution?
+    Time.now >= self.closes_for_contribution_at
+  end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -2,4 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Document, type: :model do
   it { should validate_presence_of(:body) }
+
+  describe "#closed_for_contribution?" do
+    it "should be true when now is greater than closes_for_contribution_at" do
+      subject.closes_for_contribution_at = Time.now
+      expect(subject.closed_for_contribution?).to be_truthy
+    end
+
+    it "should be false when now is lesser than closes_for_contribution_at" do
+      subject.closes_for_contribution_at = Time.now + 10.days
+      expect(subject.closed_for_contribution?).to be_falsey
+    end
+  end
 end
